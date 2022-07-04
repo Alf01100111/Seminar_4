46bdfeafd53f0791fca107116291743081b39e5a
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-04 23:20:14 +0300

    task 25

---
+++ task 25/Program.cs
@@ -0,0 +1,18 @@
+﻿
+System.Console.WriteLine("ВВедите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень ");
+int pow = Convert.ToInt32(Console.ReadLine());
+
+int Expon(int num, int power)
+{
+    int result = num;
+    for (int i = 1; i < power; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+
+int final = Expon(number,pow);
+System.Console.Write($"{number} в степени {pow} = {final}");

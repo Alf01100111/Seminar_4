53326cf411015187230b29e6b1b0a697473584dd
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-05 00:03:50 +0300

    task 27

---
+++ task 27/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int sumInNum(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result += (num%10);
+        num /= 10;
+    }
+    return result;
+}
+System.Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Сумма цифр в числе {sumInNum(number)}");

ec1ce1f8ab812b322221f2832c0ed9eb5623e336
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-06 00:02:24 +0300

    вывод массива

--- tusk 29/Program.cs
+++ tusk 29/Program.cs
@@ -1,6 +1,5 @@
 ﻿System.Console.WriteLine("Как вводим массив 1-руками, 2-случайный массив");
 int choice = Convert.ToInt32(Console.ReadLine());
-
 int[] RandomArr(int j)
 {
     Random rnd = new Random();                  //как работает рандом? спросить на семинаре почему такая кривая конструкция
@@ -16,11 +15,10 @@ void PrintArr(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        System.Console.Write(" ", arr[i]);
+        System.Console.Write($"{arr[i]},");
     }
-    System.Console.Write("\b\b");           //почему не работает затирание последнего символа?
+    System.Console.Write("\b\b");          // не работает!
 }
-
 
 int[] HandArr(int j)
 {
@@ -28,11 +26,25 @@ int[] HandArr(int j)
     int[] Arr8 = new int[j];
     for (int i = 0; i < Arr8.Length; i++)
     {
-        System.Console.Write($"{i + 1} элемент=");
+        System.Console.Write($"{i} элемент= ");
         Arr8[i] = Convert.ToInt32(Console.ReadLine());
     }
     return Arr8;
 }
-
-int[] finall = HandArr(8);
-PrintArr(finall);
+int[] result = new int[8];
+if (choice == 1)
+{
+    result = HandArr(8);
+    System.Console.WriteLine($"Ваш массив "); 
+    PrintArr(result);
+}
+if (choice == 2)
+{
+    result = RandomArr(8);
+    System.Console.WriteLine($"Ваш массив "); 
+    PrintArr(result);
+}
+if (choice !=1 && choice !=2)
+{
+    System.Console.WriteLine("неверный ввод");
+}

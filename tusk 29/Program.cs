002f674ccf1759eca124c78c66834312e942e2cd
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-05 23:20:29 +0300

    не печатает массив

---
+++ tusk 29/Program.cs
@@ -0,0 +1,38 @@
+﻿System.Console.WriteLine("Как вводим массив 1-руками, 2-случайный массив");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+int[] RandomArr(int j)
+{
+    Random rnd = new Random();                  //как работает рандом? спросить на семинаре почему такая кривая конструкция
+    int[] Arr8 = new int[j];
+    for (int i = 0; i < Arr8.Length; i++)
+    {
+        Arr8[i] = rnd.Next(0, 100);              //как работает рандом? спросить на семинаре почему такая кривая конструкция
+    }
+    return Arr8;
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(" ", arr[i]);
+    }
+    System.Console.Write("\b\b");           //почему не работает затирание последнего символа?
+}
+
+
+int[] HandArr(int j)
+{
+    System.Console.WriteLine("Введите элементы в массив поочереди -");
+    int[] Arr8 = new int[j];
+    for (int i = 0; i < Arr8.Length; i++)
+    {
+        System.Console.Write($"{i + 1} элемент=");
+        Arr8[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return Arr8;
+}
+
+int[] finall = HandArr(8);
+PrintArr(finall);
